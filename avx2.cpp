21553a93d0d4d998431deeaceafd52df035e8667
rokicki <rokicki@gmail.com>
2018-04-02 17:51:18 -0700

    Made avx2 only compile if the instructions are available.

--- avx2.cpp
+++ avx2.cpp
@@ -1,3 +1,4 @@
+#ifdef __AVX2__
 #include "lifealgo.h"
 #include "util.h"
 #include <algorithm>
@@ -166,3 +167,4 @@ int avx2algo::nextstep(int id, int n, int needpop) {
    }
    return r ;
 }
+#endif
