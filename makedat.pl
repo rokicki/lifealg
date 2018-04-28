96edc4da4e2e9c4373a5888883f38addfbe81703
rokicki <rokicki@gmail.com>
2018-04-28 09:22:43 -0700

    Updated the shootout.

--- makedat.pl
+++ makedat.pl
@@ -1,7 +1,8 @@
 $max = shift || 30 ;
 print "algo,pat,tim,gen,lgen\n" ;
-for $f (glob "lifealgo-*-2-T$max.log") {
-   $f =~ /lifealgo-(.*)-(.*)-2-T$max.log/ or die "Can't parse" ;
+for $f (glob "lifealgo-*-2-T$max*.log") {
+   $f =~ /lifealgo-(.*)-(.*)-2-T(\d+)/ or die "Can't parse" ;
+   next if $3 != $max ;
    $pat = $1 ;
    $algo = $2 ;
    open F, $f or die "Can't open" ;
@@ -14,7 +15,14 @@ for $f (glob "lifealgo-*-2-T$max.log") {
       $pop = $f[2] ;
       last if $gen < 0 || $gen > 1080000000 ;
       $lgen = int(log($gen+2)/log(2) + 0.5) ;
-      print "$algo,$pat,$tim,$gen,$lgen\n" ;
+      $key = "$algo $pat " . (100 + $lgen) ;
+      if (defined($seen{$key})) {
+         $tim = $seen{$key} if $tim > $seen{$key} ;
+      } else {
+         $seen{$key} = $tim ;
+      }
+      $data{$key} = "$algo,$pat,$tim,$gen,$lgen\n" ;
    }
    close F ;
 }
+print $data{$_} for sort keys %data ;
